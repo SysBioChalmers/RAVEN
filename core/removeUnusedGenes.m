322d41a8c8ea76a9c4cea56cdbd7fdc65add3135
Eduard Kerkhoven <eduardk@chalmers.se>
2017-05-09 22:39:41 +0200

    New function to remove unused genes
    That are not annotated to any reaction, checked via rxnGeneMat

---
+++ core/removeUnusedGenes.m
@@ -0,0 +1,40 @@
+function reducedModel=removeUnusedGenes(model)
+% removeUnusedGenes
+%   Deletes all genes that are not associated to any reaction
+%
+%   model           a model structure
+%
+%   reducedModel    an updated model structure
+%
+%   Usage: reducedModel=removeUnusedGenes(model)
+%
+%   Eduard Kerkhoven, 2017-05-09
+%
+reducedModel=model;
+
+%Find all genes that are not used
+[~, b]=find(reducedModel.rxnGeneMat);
+toKeep=false(numel(reducedModel.genes),1);
+toKeep(b)=true;
+
+reducedModel.genes=reducedModel.genes(toKeep);
+reducedModel.rxnGeneMat=reducedModel.rxnGeneMat(:,toKeep);
+
+disp('Number of unused genes removed from the model:')
+disp(numel(toKeep(~toKeep)))
+
+if isfield(reducedModel,'geneShortNames')
+   reducedModel.geneShortNames=reducedModel.geneShortNames(toKeep);
+end
+
+if isfield(reducedModel,'geneMiriams')
+   reducedModel.geneMiriams=reducedModel.geneMiriams(toKeep);
+end
+
+if isfield(reducedModel,'geneFrom')
+   reducedModel.geneFrom=reducedModel.geneFrom(toKeep);
+end
+
+if isfield(reducedModel,'geneComps')
+   reducedModel.geneComps=reducedModel.geneComps(toKeep);
+end
