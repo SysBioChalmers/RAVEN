c68e43d821e0a7a8eb526888cb56e2944e2a154a
Simonas Marcišauskas <simmarc@chalmers.se>
2017-11-22 14:56:26 +0100

    Fix importModel
    Sometimes metabolite ids contain compartment information between the square brackets. Now this information is removed, if the metabolite ids are still unique after compartment information removal

--- io/importModel.m
+++ io/importModel.m
@@ -1002,6 +1002,18 @@ if ~isfield(model,'genes') && isfield(model,'grRules')
    model=rmfield(model,'grRules');
 end
 
+%The metabolite ids may still contain the compartment abbreviations in the
+%end, so these are removed. However, such removal is only saved if the
+%metabolite ids are still unique;
+for i=1:numel(model.comps)
+    tempMets=model.mets;
+    tempMets=regexprep(tempMets,'\]$','');
+    tempMets=regexprep(tempMets,['\[', model.comps{i}, '$'],'');
+    if numel(unique(tempMets))==numel(model.mets)
+        model.mets=tempMets;
+    end;
+end;
+
 %Print warnings about bad structure
 if supressWarnings==false
     checkModelStruct(model,false);
