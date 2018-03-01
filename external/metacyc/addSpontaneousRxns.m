350fb02c3a375a531f9a0df1bd1cf96078a3ed59
Hao-Chalmers <hao.wang@chalmers.se>
2018-03-01 12:05:46 +0100

    fix: reformat tabs

--- external/metacyc/addSpontaneousRxns.m
+++ external/metacyc/addSpontaneousRxns.m
@@ -35,8 +35,8 @@ pathways=unique(pathways);
 % Genearte the matirx, row: spontaneousRxns, column: pathways
 sprxnPwyMat=zeros(numel(isSpontaneous),numel(pathways));
 for i=1:numel(isSpontaneous)
-	[a, b]=ismember(isSpontaneous{i},metaCycRxns.rxns);
-	if ~isempty(metaCycRxns.pwys{b})
+    [a, b]=ismember(isSpontaneous{i},metaCycRxns.rxns);
+    if ~isempty(metaCycRxns.pwys{b})
         [crap, indexes]=ismember(strsplit(metaCycRxns.pwys{b},';'),pathways);
         sprxnPwyMat(i,indexes)=1;   
 	end
@@ -45,42 +45,42 @@ end
 % Go through the rxnList and obtain relevant pathways
 pwys={};
 for i=1:numel(rxnList)
-	[a, b]=ismember(rxnList{i},metaCycRxns.rxns);
+    [a, b]=ismember(rxnList{i},metaCycRxns.rxns);
 	if a && ~isempty(metaCycRxns.pwys{b})
         pwys=[pwys;transpose(strsplit(metaCycRxns.pwys{b},';'))];
-	end
+    end
 end
 pwys=unique(pwys);
 
 % Get spontaneous reactions associated with the pathways
 hits=[];
 for i=1:numel(pwys)
-		[a, b]=ismember(pwys{i},pathways);
-		if ~isempty(find(sprxnPwyMat(:,b)))
-				hits=[hits;find(sprxnPwyMat(:,b))];
-		end		
+    [a, b]=ismember(pwys{i},pathways);
+    if ~isempty(find(sprxnPwyMat(:,b)))
+        hits=[hits;find(sprxnPwyMat(:,b))];
+    end		
 end
 spontaneousRxnList=isSpontaneous(unique(hits));
 
 % Check if the reactants/products are included in metList
 for i=1:numel(spontaneousRxnList)
-	[a, b]=ismember(spontaneousRxnList{i},metaCycRxns.rxns);
-		
-	%Obtain the reactants and products
-	reactants=all(ismember(metaCycRxns.mets(find(metaCycRxns.S(:,b)==-1)),metList));
-	products=all(ismember(metaCycRxns.mets(find(metaCycRxns.S(:,b)==1)),metList));
-		
-	%Either reactants or products should be present for reversible reaction
-	if metaCycRxns.rev(b)==1
+    [a, b]=ismember(spontaneousRxnList{i},metaCycRxns.rxns);
+
+    %Obtain the reactants and products
+    reactants=all(ismember(metaCycRxns.mets(find(metaCycRxns.S(:,b)==-1)),metList));
+    products=all(ismember(metaCycRxns.mets(find(metaCycRxns.S(:,b)==1)),metList));
+
+    %Either reactants or products should be present for reversible reaction
+    if metaCycRxns.rev(b)==1
         if ~(reactants || products)
             spontaneousRxnList{i}=[];
         end
-	%Reactants should be present for irreversible reactions
+    %Reactants should be present for irreversible reactions
     else
         if ~reactants
             spontaneousRxnList{i}=[];
         end	
-	end
+    end
 end
 spontaneousRxnList=spontaneousRxnList(~cellfun(@isempty, spontaneousRxnList));
 
