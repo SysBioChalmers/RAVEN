SysBioChalmers/RAVEN

commit a390f39c64a695a809990e4767ee1bf2804e600a

refactor: importModel location of gene association
- genes can be stored in three different locations, instead of looking for all three locations, go through them in priority.
- first priority is fbc_geneProductAssociation, second priority is in NOTES, third priority is in MODIFIER

io/importModel.m
@@ -494,7 +494,28 @@ for i=1:numel(modelSBML.reaction)
     end
     
     %Find the associated gene if available
-    if isfield(modelSBML.reaction(i),'modifier')
+        %If FBC, get gene association data from corresponding fields
+    if isfield(modelSBML.reaction(i),'fbc_geneProductAssociation')
+        if ~isempty(modelSBML.reaction(i).fbc_geneProductAssociation) && ~isempty(modelSBML.reaction(i).fbc_geneProductAssociation.fbc_association)
+            grRules{counter}=modelSBML.reaction(i).fbc_geneProductAssociation.fbc_association.fbc_association;
+        end
+    elseif isfield(modelSBML.reaction(i),'notes')
+    %This section was previously executed only if isSBML2COBRA is true. Now
+    %it will be executed, if 'GENE_ASSOCIATION' is found in
+    %modelSBML.reaction(i).notes
+        if strfind(modelSBML.reaction(i).notes,'GENE_ASSOCIATION')
+            geneAssociation=parseNote(modelSBML.reaction(i).notes,'GENE_ASSOCIATION');
+        elseif strfind(modelSBML.reaction(i).notes,'GENE ASSOCIATION')
+            geneAssociation=parseNote(modelSBML.reaction(i).notes,'GENE ASSOCIATION');
+        else
+            geneAssociation='';
+        end
+        if ~isempty(geneAssociation)
+        %This adds the grRules. The gene list and rxnGeneMat are created
+        %later
+            grRules{counter}=geneAssociation;
+        end
+    elseif isfield(modelSBML.reaction(i),'modifier')
         if ~isempty(modelSBML.reaction(i).modifier)
             rules='';
             for j=1:numel(modelSBML.reaction(i).modifier)
@@ -549,32 +570,7 @@ for i=1:numel(modelSBML.reaction)
             grRulesFromModifier{counter}=rules;%Backup copy for grRules, useful to parse Yeast 7.6
         end
     end
-    %This section was previously executed only if isSBML2COBRA is true. Now
-    %it will be executed, if 'GENE_ASSOCIATION' is found in
-    %modelSBML.reaction(i).notes
-    if isfield(modelSBML.reaction(i),'notes')
-        if strfind(modelSBML.reaction(i).notes,'GENE_ASSOCIATION')
-            geneAssociation=parseNote(modelSBML.reaction(i).notes,'GENE_ASSOCIATION');
-        elseif strfind(modelSBML.reaction(i).notes,'GENE ASSOCIATION')
-            geneAssociation=parseNote(modelSBML.reaction(i).notes,'GENE ASSOCIATION');
-        else
-            geneAssociation='';
-        end
-    end
-    
-    if ~isempty(geneAssociation)
-        %This adds the grRules. The gene list and rxnGeneMat are created
-        %later
-        grRules{counter}=geneAssociation;
-    end
-    
-    %If FBC, get gene association data from corresponding fields
-    if isfield(modelSBML.reaction(i),'fbc_geneProductAssociation')
-        if ~isempty(modelSBML.reaction(i).fbc_geneProductAssociation) && ~isempty(modelSBML.reaction(i).fbc_geneProductAssociation.fbc_association)
-            grRules{counter}=modelSBML.reaction(i).fbc_geneProductAssociation.fbc_association.fbc_association;
-        end
-    end
-    
+   
     %Add reaction compartment
     if isfield(modelSBML.reaction(i),'compartment')
         if ~isempty(modelSBML.reaction(i).compartment)

commit 1a45b229d42f333b8e1c3c330f497d7317ca738f

fix: importModel extracting grRules from non-FBCv2 models
- commit a390f39c64a695a809990e4767ee1bf2804e600a introduced a bug, where only 'notes' was checked for grRules, and never the 'modifier' field. Now it checks if no grRules is defined yet, and in that case checks in the 'modifier' field.

io/importModel.m
@@ -515,60 +515,59 @@ for i=1:numel(modelSBML.reaction)
             %later
             grRules{counter}=geneAssociation;
         end
-    elseif isfield(modelSBML.reaction(i),'modifier')
-        if ~isempty(modelSBML.reaction(i).modifier)
-            rules='';
-            for j=1:numel(modelSBML.reaction(i).modifier)
-                modifier=modelSBML.reaction(i).modifier(j).species;
-                if ~isempty(modifier)
-                    if strcmpi(modifier(1:2),'E_')
-                        index=find(strcmp(modifier,geneIDs));
-                        %This should be unique and in the geneIDs list,
-                        %otherwise something is wrong
-                        if numel(index)~=1
-                            EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
-                            dispEM(EM);
-                        end
+    end
+    if isempty(grRules{counter}) && ~isempty(modelSBML.reaction(i).modifier)
+        rules='';
+        for j=1:numel(modelSBML.reaction(i).modifier)
+            modifier=modelSBML.reaction(i).modifier(j).species;
+            if ~isempty(modifier)
+                if strcmpi(modifier(1:2),'E_')
+                    index=find(strcmp(modifier,geneIDs));
+                    %This should be unique and in the geneIDs list,
+                    %otherwise something is wrong
+                    if numel(index)~=1
+                        EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
+                        dispEM(EM);
+                    end
+                    if ~isempty(rules)
+                        rules=[rules ' or (' geneNames{index} ')'];
+                    else
+                        rules=['(' geneNames{index} ')'];
+                    end
+                elseif strcmp(modifier(1:2),'s_')
+                    index=find(strcmp(modifier,metaboliteIDs));
+                    %This should be unique and in the geneIDs list,
+                    %otherwise something is wrong
+                    if numel(index)~=1
+                        EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
+                        dispEM(EM);
+                    end
+                    if ~isempty(rules)
+                        rules=[rules ' or (' metaboliteIDs{index} ')'];
+                    else
+                        rules=['(' metaboliteIDs{index} ')'];
+                    end
+                else
+                    %It seems to be a complex. Add the corresponding
+                    %genes from the name of the complex (not the
+                    %reaction that creates it)
+                    index=find(strcmp(modifier,complexIDs));
+                    if numel(index)==1
                         if ~isempty(rules)
-                            rules=[rules ' or (' geneNames{index} ')'];
+                            rules=[rules ' or (' strrep(complexNames{index},':',' and ') ')'];
                         else
-                            rules=['(' geneNames{index} ')'];
-                        end
-                    elseif strcmp(modifier(1:2),'s_')
-                        index=find(strcmp(modifier,metaboliteIDs));
-                        %This should be unique and in the geneIDs list,
-                        %otherwise something is wrong
-                        if numel(index)~=1
-                            EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
-                            dispEM(EM);
-                        end
-                        if ~isempty(rules)
-                            rules=[rules ' or (' metaboliteIDs{index} ')'];
-                        else
-                            rules=['(' metaboliteIDs{index} ')'];
+                            rules=['(' strrep(complexNames{index},':',' and ') ')'];
                         end
                     else
-                        %It seems to be a complex. Add the corresponding
-                        %genes from the name of the complex (not the
-                        %reaction that creates it)
-                        index=find(strcmp(modifier,complexIDs));
-                        if numel(index)==1
-                            if ~isempty(rules)
-                                rules=[rules ' or (' strrep(complexNames{index},':',' and ') ')'];
-                            else
-                                rules=['(' strrep(complexNames{index},':',' and ') ')'];
-                            end
-                        else
-                            %Could not find a complex
-                            EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
-                            dispEM(EM);
-                        end
+                        %Could not find a complex
+                        EM=['Could not get the gene association data from reaction ' reactionIDs{i}];
+                        dispEM(EM);
                     end
                 end
             end
-            grRules{counter}=rules;
-            grRulesFromModifier{counter}=rules;%Backup copy for grRules, useful to parse Yeast 7.6
         end
+        grRules{counter}=rules;
+        grRulesFromModifier{counter}=rules;%Backup copy for grRules, useful to parse Yeast 7.6
     end
     
     %Add reaction compartment