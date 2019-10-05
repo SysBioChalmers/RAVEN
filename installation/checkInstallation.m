f423fb0031783cbd9491aac3590cbae7779d0268
Simonas Marcišauskas <simmarc@chalmers.se>
2019-10-05 21:55:30 +0200

    fix: checkInstallation classify external binaries + fix the printed messages
    1. Divide external binaries into two categories. The essential binaries (makeblastdb, blastp, hmmsearch) are the ones which are used by most of the users, while the remaining ones (cd-hit, mafft, hmmsearch) are only used if KEGG FTP dump files are parsed in getKEGGModelForOrganism. The latter category is therefore not essential.
    2. Fix the printed messages to increas the clarity of the printed output.
    3. Change "Matlab" to "MATLAB".

--- installation/checkInstallation.m
+++ installation/checkInstallation.m
@@ -3,14 +3,14 @@ function checkInstallation()
 %   The purpose of this function is to check if all necessary functions are
 %   installed and working. It also checks whether there are any functions
 %   with overlapping names between RAVEN and other toolboxes or
-%   user-defined functions, which are accessible from Matlab pathlist
+%   user-defined functions, which are accessible from MATLAB pathlist
 %
 %   Usage: checkInstallation()
 %
-%	Simonas Marcisauskas, 2019-10-04
+%	Simonas Marcisauskas, 2019-10-05
 %
 
-%Check if RAVEN is in the Matlab path list
+%Check if RAVEN is in the MATLAB path list
 paths=textscan(path,'%s','delimiter', pathsep);
 paths=paths{1};
 
@@ -25,10 +25,11 @@ else
     fprintf('\n*** THE RAVEN TOOLBOX - DEVELOPMENT VERSION ***\n\n');
 end
 
+fprintf('Checking if RAVEN is on the MATLAB path... ');
 if ismember(ravenDir,paths)
-    fprintf('Checking if RAVEN is on the Matlab path... PASSED\n');
+    fprintf('OK\n');
 else
-    fprintf('Checking if RAVEN is on the Matlab path... FAILED\n');
+    fprintf('Not OK\n');
     addMe=input('Would you like to add the RAVEN directory to the path list? Y/N\n','s');
     if strcmpi(addMe,'y')
         subpath=regexp(genpath(ravenDir),pathsep,'split'); %List all subdirectories
@@ -46,26 +47,29 @@ xmlFile=fullfile(ravenDir,'tutorial','empty.xml');
 matFile=fullfile(ravenDir,'tutorial','empty.mat');
 
 %Check if it is possible to parse an Excel file
+fprintf('Checking if it is possible to parse a model in Microsoft Excel format... ');
 try
     importExcelModel(excelFile,false,false,true);
-    fprintf('Checking if it is possible to parse a model in Microsoft Excel format... PASSED\n');
+    fprintf('OK\n');
 catch
-    fprintf('Checking if it is possible to parse a model in Microsoft Excel format... FAILED\n');
+    fprintf('Not OK\n');
 end
 
 %Check if it is possible to import an SBML model using libSBML
+fprintf('Checking if it is possible to import an SBML model using libSBML... ');
 try
     importModel(xmlFile);
     try
         libSBMLver=OutputSBML; % Only works in libSBML 5.17.0+
-        fprintf('Checking if it is possible to import an SBML model using libSBML... PASSED\n');
+        fprintf('OK\n');
     catch
-        fprintf(['Checking if it is possible to import an SBML model using libSBML... PASSED\n\n'...
+        fprintf(['Not OK\n\n'...
             'An older libSBML version was found, update to version 5.17.0 or higher\n'...
-            'for a significant improvement of model import.\n\n']);
+            'for a significant improvement of model import\n\n']);
     end
 catch
-    fprintf('Checking if it is possible to import an SBML model using libSBML... FAILED\nTo import SBML models, download libSBML from http://sbml.org/Software/libSBML/Downloading_libSBML and add to MATLAB path\n');
+    fprintf(['Not OK\nTo import SBML models, download libSBML from\n'...
+        'http://sbml.org/Software/libSBML/Downloading_libSBML and add to MATLAB path\n']);
 end
 
 %Define values for keepSolver and workingSolvers, needed for solver
@@ -86,17 +90,18 @@ end
 solver={'gurobi','mosek','cobra'};
 
 for i=1:numel(solver)
+    fprintf(['Checking if it is possible to solve an LP problem using ',solver{i},'... ']);
     try
         setRavenSolver(solver{i});
         load(matFile);
         solveLP(empty);
         workingSolvers=strcat(workingSolvers,';',solver{i});
-        fprintf(['Checking if it is possible to solve an LP problem using ',solver{i},'... PASSED\n']);
+        fprintf('OK\n');
         if strcmp(curSolv,solver{i})
             keepSolver=true;
         end
     catch
-        fprintf(['Checking if it is possible to solve an LP problem using ',solver{i},'... FAILED\n']);
+        fprintf('Not OK\n');
     end
 end
 
@@ -117,7 +122,9 @@ else
     %No functional solvers were found, so the setting is restored back to
     %original
     setRavenSolver(curSolv);
-    fprintf('WARNING: No working solver was found!\nInstall the solver, set it using setRavenSolver(''solverName'') and run checkInstallation again.\nAvailable solverName options are ''mosek'', ''gurobi'' and ''cobra''\n\n');
+    fprintf(['WARNING: No working solver was found!\n'...
+        'Install the solver, set it using setRavenSolver(''solverName'') and run checkInstallation again\n'...
+        'Available solverName options are ''mosek'', ''gurobi'' and ''cobra''\n\n']);
 end
 
 if ~ispc
@@ -126,50 +133,60 @@ if ~ispc
     elseif isunix
         binEnd='';
     end
-    fprintf('Checking binary executables..\n');
-    [res,~]=system(['"' fullfile(ravenDir,'software','blast+',['blastp' binEnd]) '"']);
-    if res==1
-        fprintf(['Checking blastp' binEnd '.. OK\n']);
-    else
-        fprintf(['Checking blastp' binEnd '.. Not OK! The binary must be recompiled from source before running RAVEN\n']);
-    end
+    fprintf('Checking essential binary executables:\n');
+    fprintf('NOTE: Broken binary executables <strong>must be fixed</strong> before running RAVEN\n');    
+    fprintf(['\tmakeblastdb' binEnd '... ']);
     [res,~]=system(['"' fullfile(ravenDir,'software','blast+',['makeblastdb' binEnd]) '"']);
     if res==1
-        fprintf(['Checking makeblastdb' binEnd '.. OK\n']);
+        fprintf('OK\n');
     else
-        fprintf(['Checking makeblastdb' binEnd '.. Not OK! The binary must be recompiled from source before running RAVEN\n']);
+        fprintf('Not OK! Download/compile the binary and run checkInstallation again\n');
     end
-    [res,~]=system(['"' fullfile(ravenDir,'software','cd-hit',['cd-hit' binEnd]) '"']);
+    fprintf(['\tblastp' binEnd '... ']);
+    [res,~]=system(['"' fullfile(ravenDir,'software','blast+',['blastp' binEnd]) '"']);
     if res==1
-        fprintf(['Checking cd-hit' binEnd '.. OK\n']);
+        fprintf('OK\n');
     else
-        fprintf(['Checking cd-hit' binEnd '.. Not OK! The binary must be recompiled from source before running RAVEN\n']);
+        fprintf('Not OK! Download/compile the binary and run checkInstallation again\n');
     end
-    [res,~]=system(['"' fullfile(ravenDir,'software','hmmer',['hmmbuild' binEnd]) '"']);
-    if res==1
-        fprintf(['Checking hmmbuild' binEnd '.. OK\n']);
-    else
-        fprintf(['Checking hmmbuild' binEnd '.. Not OK! The binary must be recompiled from source before running RAVEN\n']);
-    end
+    fprintf(['\thmmsearch' binEnd '... ']);
     [res,~]=system(['"' fullfile(ravenDir,'software','hmmer',['hmmsearch' binEnd]) '"']);
     if res==1
-        fprintf(['Checking hmmsearch' binEnd '.. OK\n']);
+        fprintf('OK\n');
     else
-        fprintf(['Checking hmmsearch' binEnd '.. Not OK! The binary must be recompiled from source before running RAVEN\n']);
+        fprintf('Not OK! Download/compile the binary and run checkInstallation again\n');
     end
+    fprintf('Checking non-essential/development binary executables:\n');
+    fprintf('NOTE: Only fix these binaries if planning to use KEGG FTP dump files in getKEGGModelForOrganism\n');
+    fprintf(['\tcd-hit' binEnd '... ']);
+    [res,~]=system(['"' fullfile(ravenDir,'software','cd-hit',['cd-hit' binEnd]) '"']);
+    if res==1
+        fprintf('OK\n');
+    else
+        fprintf('Not OK! If necessary, download/compile the binary and run checkInstallation again\n');
+    end
+    fprintf('\tmafft.bat... ');
     if ismac
         [res,~]=system(['"' fullfile(ravenDir,'software','mafft','mafft-mac','mafft.bat') '" --help ']);
     elseif unix
         [res,~]=system(['"' fullfile(ravenDir,'software','mafft','mafft-linux64','mafft.bat') '" --help ']);
     end
     if res==1
-        fprintf('Checking mafft.bat.. OK\n\n');
+        fprintf('OK\n');
     else
-        fprintf('Checking mafft.bat.. Not OK! The binary must be recompiled from source before running RAVEN\n\n');
+        fprintf('Not OK! If necessary, download/compile the binary and run checkInstallation again\n');
+    end
+    fprintf(['\thmmbuild' binEnd '... ']);
+    [res,~]=system(['"' fullfile(ravenDir,'software','hmmer',['hmmbuild' binEnd]) '"']);
+    if res==1
+        fprintf('OK\n\n');
+    else
+        fprintf('Not OK! If necessary, download/compile the binary and run checkInstallation again\n');
     end
 end
 
-fprintf('Checking the uniqueness of RAVEN functions across Matlab path...\n');
+fprintf('Checking whether RAVEN functions are non-redundant across MATLAB path... ');
 checkFunctionUniqueness();
 
+fprintf('\n*** checkInstallation complete ***\n\n');
 end
