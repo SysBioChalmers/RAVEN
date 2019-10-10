c013e00b48f5ca1076bad3b32413a20f7edadbc4
Eduard Kerkhoven <eduardk@chalmers.se>
2019-10-10 15:20:06 +0200

    feat: remove setting 'mosek' as solver

--- installation/checkInstallation.m
+++ installation/checkInstallation.m
@@ -77,7 +77,7 @@ else
 end
 
 %Check if it is possible to solve an LP problem using different solvers
-solver={'gurobi','mosek','cobra'};
+solver={'gurobi','cobra'};
 
 for i=1:numel(solver)
     try
