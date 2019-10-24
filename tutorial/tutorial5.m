4ac6d855db921df8a3668e034b88f863495edaf2
Simonas Marcišauskas <simmarc@chalmers.se>
2019-10-24 21:47:08 +0200

    doc: move tutorial4 -> tutorial5
    - remove the "Exercise" definition and use only "Tutorial" instead
     - re-order tutorials from 1 to 6 rather than from 0 to 5

--- tutorial/tutorial5.m
+++ tutorial/tutorial5.m
@@ -1,152 +1,187 @@
 % tutorial5
-%   This exercise demonstrates how to reconstruct a combined draft GEM by
-%   from KEGG and MetaCyc pathway databases. A combined model with the
-%   comprehensive coverage of metabolic pathways is generated from
-%   different de novo reconstruction approaches. The input is a FASTA
-%   format file with whole-proteome sequences. The combined model is
-%   subsequently used for refinement of existing high-quality model and
-%   generation of a new version of GEM, by utilizing the manual curation
-%   results. This tutorial is a showcase of the new features released in
-%   RAVEN 2.0 through demonstrating the utilization of the newly developed
-%   functions on GEM reconstruction and curation for Streptomyces
-%   coelicolor strain A3(2). Users may apply this script as the template in
-%   their own work for other organisms.
-%   This refers to Exercise 5 from "RAVEN tutorials.docx"
+%   This exercise is about creating a model from KEGG, based on protein
+%   sequences in a FASTA file, and doing some functionality checks on the
+%   model. The example case is for the yeast Saccharomyces cerevisiae. This
+%   tutorial is more of a showcase than the previous four, and its main
+%   purpose is to serve as a scaffold to reconstruct a GEM for any
+%   organism.
+%   This refers to Tutorial 5 from "RAVEN tutorials.docx"
 %
-%   Simonas Marcisauskas, 2019-10-21
+%   Simonas Marcisauskas, 2019-09-24
 %
 
-%Before reconstruction, a FASTA file with protein sequences of the target
-%organism needs to be prepared. In this tutorial, all protein sequences of
-%S. coelicolor A3(2) were downloaded from the NCBI genome database and
-%provided in current folder (Sco_all_protein.faa). The description lines
-%(starting with ">" character) of this FASTA file were modified by keeping
-%only the locus tag (e.g. SCO6005), which is commonly used as the gene
-%names in GEMs.
+%Start by downloading trained Hidden Markov Models for eukaryotes. This can
+%be done automatically or manually from the RAVEN Wiki in its GitHub
+%repository. In this tutorial, the archive "euk100_kegg91" is picked for
+%the automatic download. See the documentation in the RAVEN Wiki for more
+%information regarding preparation of such archive.
 
-%Generate a draft GEM from MetaCyc database. The first parameter is
-%organismID and needs to be specified by user. The other parameters are set
-%to exclude unbalanced and undetermined reactions, but keep transport
-%reactions. The two parameters for homology search are set to the default
-%values that have been optimized to capture the protein hits with both
-%comprehensive coverage and the least false positives.
-ScoMetaCycDraftModel=getMetaCycModelForOrganism('ScoMetaCyc','Sco_all_protein.faa',1);
+%This creates a model for S. cerevisiae. The parameters are set to exclude
+%general or unclear reactions and reactions with undefined stoichiometry.
+%Type "help getKEGGModelForOrganism" to see what the different parameters
+%are for. This process takes up to 20-35 minutes in macOS, Unix systems and
+%40-55 minutes in Windows, depending on your hardware and the size of
+%target organism proteome
+model=getKEGGModelForOrganism('sce','sce.fa','euk100_kegg91','output',false,false,false,false,10^-30,0.8,0.3,-1);
 
-%Generate the two draft models from KEGG database. The first one is
-%reconstructed from the genome information annotated by KEGG. Since KEGG
-%provides genome annotations for over 5000 organisms, their GEMs can thus
-%be generated without providing protein sequences while only using the
-%organismID. The organisms with KEGG annotation and their associated ids
-%(e.g. 'sco' for Streptomyces coelicolor) can be found from here:
-%https://www.kegg.jp/kegg/catalog/org_list.html.
-%Generate a draft model using KEGG annotation and exclude incomplete
-%reactions and reactions with undefined stoichiometry
-ScoKEGGAnnotation=getKEGGModelForOrganism('sco','','','',1,0,0);
+%The resulting model should contain around 1574 reactions, 1601
+%metabolites and 821 genes. Small variations are possible since it is an
+%heuristic algorithm.
+disp(model);
 
-%Generate the second KEGG-based draft model based on sequence homology to
-%KEGG Ortholog sequence clusters while excluding incomplete reactions and
-%the ones with undefined stoichiometry. Type "help getKEGGModelForOrganism"
-%to see the detailed instructions for the choice of different parameters.
-%The default values for homology search are used because they have been
-%optimized for the best performance.
-ScoKEGGHomology=getKEGGModelForOrganism('ScoKEGGHMMs','Sco_all_protein.faa','prok90_kegg91','',1,0,0);
+%A first control is that the model should not be able to produce any
+%metabolites without uptake of some metabolites. This commonly happens when
+%metabolites have a different meaning in different reactions. The best way
+%to find such reactions is to run makeSomething and analyze the resulting
+%solution for bad reactions. An automated approach is to use removeBadRxns,
+%which tries to do the same thing in an automated manner. Type
+%"help removeBadRxns" for details.
+[newModel, removedRxns]=removeBadRxns(model);
 
-%De novo reconstruction from MetaCyc should take about 10 minutes, while
-%both reconstructions from KEGG may take up to 50-60 minutes in overall
-%depending on the computer hardware and the system used. Given that KEGG
-%and MetaCyc databases are formulated in different ways; KEGG relies on
-%sequence-based annotation, while MetaCyc collects only experimentally
-%verified pathways. Therefore, integration of MetaCyc- and KEGG-based draft
-%models could have a better coverage of the metbolism for the target
-%organism.
+%One can see an error about that H+ can be made even if no reactions were
+%unbalanced. Protons are particularly problematic since it is rather
+%arbitary at which pH the formulas are written for. For the purpose of this
+%analysis, the protons can be ignored and fixed later.
+[newModel, removedRxns]=removeBadRxns(model,1,{'H+'},true);
 
-%At first, the two KEGG-based models can be directly merged
-ScoKEGGDraftModel=mergeModels({ScoKEGGAnnotation ScoKEGGHomology});
+%Only one reaction was removed because it enabled the model to produce
+%something from nothing. Since it is only one reaction, it might be
+%worthwhile to look into this in more detail.
+disp(removedRxns);
 
-disp(numel(ScoKEGGHomology.rxns)+numel(ScoKEGGAnnotation.rxns));
-disp(numel(ScoKEGGDraftModel.rxns));
-%By checking the reaction number, it can be seen that reaction number in
-%the merged model equals adding up the reaction numbers in homology and
-%annotation KEGG draft models. And there are duplicated reactions in this
-%merged model.
+%According to the information in KEGG about this reaction it is a general
+%polymer reaction. One might want to look at the flux distributions
+%in more detail to try to find out if there is any better alternative to
+%delete. Use makeSomething to do this.
+[fluxes, metabolite]=makeSomething(model,{'H+'},true);
+model.metNames(metabolite)
 
-%Merge the duplicated reactions into one and combine multiple iso-enzymes
-%into a single grRules. The expandModel and contractModel functions are
-%utilised, see their instructions for details.
-ScoKEGGDraftModel=expandModel(ScoKEGGDraftModel);
-ScoKEGGDraftModel=contractModel(ScoKEGGDraftModel);
+%The model could produce H2O using the following reactions
+printFluxes(model, fluxes, false, [], [],'%rxnID (%rxnName):\n\t%eqn: %flux\n')
 
-%In the end, KEGG- and MetaCyc-based draft models can be further combined
-%into an integrated GEM with 2605 reactions, 3005 metabolites and 2175
-%genes. This step is achieved by the function combineMetaCycKEGGModels,
-%which firstly converts metabolite and reaction identifiers in the KEGG
-%model into corresponding MetaCyc ids, and then detects duplications and
-%keeps only unique reactions and metabolites that are mostly in MetaCyc
-%identifiers.
-ScoCombinedDraftModel=combineMetaCycKEGGModels(ScoMetaCycDraftModel, ScoKEGGDraftModel);
+%That resulted in quite a lot of fluxes to look through. Maybe it is easier
+%if the elementally balanced reactions are excluded.
+balanceStructure=getElementalBalance(model);
 
-%With this combined model, the existing iMK1208 model is refined by
-%incorporating the new pathways/reactions that are found in the combined
-%model but absent from the previous iMK1208 model. A total of 398 reactions
-%in the combined draft model were determined as new pathways based on
-%manual curation results, which have been organized into TableS3. Load
-%these manually selected reactions and their subSystems as the
-%pre-processed array structure selectedNewRxns.
-load('iMK1208+suppInfo.mat','selectedNewRxns');
-disp(selectedNewRxns);
+%The hydrogen balancing is a bit tricky, so this time it seems more
+%relevant to only look at the ones unbalanced for oxygen (since water was
+%produced)
+goodOnes=balanceStructure.leftComp(:,6)==balanceStructure.rightComp(:,6);
+printFluxes(removeReactions(model,goodOnes), fluxes(~goodOnes), false, [], [],'%rxnID (%rxnName):\n\t%eqn: %flux\n');
 
-%Generate a sub-model that includes only these new reactions. This is
-%implemented by subtracting the other reactions from the combined model
-%using function removeReactions
-rxnsToRemove=setdiff(ScoCombinedDraftModel.rxns,selectedNewRxns.rxns);
-newRxnSubModel=removeReactions(ScoCombinedDraftModel,rxnsToRemove,1,1);
+%There is still a good number of reactions. Leave only the reactions which
+%involve amylose or starch, from one of the problematic reactions that was
+%identified earlier.
+printFluxes(model, fluxes, false, [], [],'%rxnID (%rxnName):\n\t%eqn: %flux\n',{'Amylose';'Starch'});
 
-%Now this newRxnSubModel contains only these new reactions. It needs to be
-%modified before merging with the iMK1208. Since these reactions are
-%metabolic ones and can all be assigned to the cytoplasm compartment
-newRxnSubModel.comps{1}='c';
-newRxnSubModel.compNames{1}='Cytoplasm';
+%There are two elementally unbalanced reactions, including, the reaction
+%which was identified by removeBadRxns. When looking to these reactions
+%closer, one can notice the contradiction between the reactions. The first
+%one shows that amylose and starch are interconvertible, while the second
+%reaction shows that amylose contains one less glucose unit than starch.
+%This type of general reactions are problematic and should be fixed
+%manually. It is hereby chosen to trust removeBadRxns and delete R02110.
+model=removeReactions(model,'R02110');
 
-%Amend to the genes and rxnGeneMat fields
-newRxnSubModel.genes={};
-for k=1:numel(newRxnSubModel.rxns)
-   newRxnSubModel.genes=[newRxnSubModel.genes;transpose(strsplit(newRxnSubModel.grRules{k},' or '))];
-end
-newRxnSubModel.genes=unique(newRxnSubModel.genes);
-[~, newRxnSubModel.rxnGeneMat, ~]=standardizeGrRules(newRxnSubModel);
+%The model can no longer make something from nothing. Can it consume
+%something without any output?
+[solution, metabolite]=consumeSomething(model,{'H+'},true);
+model.metNames(metabolite)
 
-%Since the newRxnSubModel is ready for incorportation, the iMK1208 model
-%can be loaded for integration
-load('iMK1208+suppInfo.mat','iMK1208');
+%Nope, so that was good. Add some uptakes and see what it can produce.
+[~, J]=ismember({'D-Glucose';'H2O';'Orthophosphate';'Oxygen';'NH3';'Sulfate'},model.metNames);
+[model, addedRxns]=addExchangeRxns(model,'in',J);
 
-%It should be noted that the incompatible nomenclatures (especially the
-%metabolite identifiers) used in different GEMs and databases led to a
-%serious problem in model comparison, curation and integration. In order to
-%properly integrate the iMK1208 with this sub-model, both models should be
-%unified into the same name space, since the metabolites in the sub-model
-%use MetaCyc identifiers that are different from the ones used in iMK1208
-%model. TableS2 contains the results for database mining and intensive
-%manual curation while associating these metabolite identifiers. Load
-%these manually selected reactions and their subSystems as the
-%pre-processed array structure metaCycMetsIniMK.
-load('iMK1208+suppInfo.mat','metaCycMetsIniMK');
+%Check which metabolites can be produced given these uptakes. The
+%canProduce function allows for output of all metabolites. This will not
+%happen in the real cell, but it is very useful for functionality testing
+%of the model. Once it is functional, the excretion reactions based on
+%evidence can be added as well.
+I=canProduce(model);
 
-%Replace the metabolites in the sub-model with the identifiers and names
-%used in iMK1208 according to the mapping information in TableS2
-%(metaCycMetsIniMK).
-[a, b]=ismember(newRxnSubModel.mets,metaCycMetsIniMK);
-I=find(a);
-newRxnSubModel.mets=strcat(newRxnSubModel.mets,'_c');
-newRxnSubModel.metNames(I)=iMK1208.metNames(b(I));
-newRxnSubModel.mets(I)=iMK1208.mets(b(I));
+fprintf('%d%%\n', round(sum(I)/numel(model.mets)*100));
+%It seems that 28% of the metabolites could be synthesized. It is not
+%directly clear whether this is a high or low number, many metabolites
+%should not be possible to synthesize from those simple precursors.
 
-%The necessary issues for merging iMK1208 with the new reactions determined
-%from de novo generated draft GEMs have been resolved. Those can be
-%directly merged into the Sco4 model, which refers to the 4th major update
-%of the GEM for Streptomyces coelicolor.
-Sco4=mergeModels({iMK1208 newRxnSubModel});
+%Try to fill gaps using the full KEGG model to see if that gives a
+%significantly higher number
+keggModel=getModelFromKEGG([],false,false,false,false);
 
-%Check out if the newly generated Sco4 could grow
-sol=solveLP(Sco4);
-disp(sol.f);
-%The results indicate that this new model is functional
+%The KEGG model is associated to more than 4,000,000 genes. They will not
+%be used for the gapfilling, so they are removed to make this a little
+%faster
+keggModel=rmfield(keggModel,'genes');
+keggModel=rmfield(keggModel,'rxnGeneMat');
+
+%It is already known from the previous commands that there are some
+%unbalanced reactions in KEGG. Only use the balanced ones for the gap
+%filling
+balanceStructure=getElementalBalance(keggModel);
+keggModel=removeReactions(keggModel,balanceStructure.balanceStatus~=1,true,true);
+
+%The function fillGaps with these settings will try to include reactions in
+%order to have flux through all reactions in the model. There are other
+%settings as well. The first flag says that production of all metabolites
+%should be allowed.
+params.relGap=0.6; %Lower number for a more exhaustive search
+params.printReport=true;
+[newConnected, cannotConnect, addedRxns, newModel, exitFlag]=fillGaps(model,keggModel,true,false,false,[],params);
+
+%The results show that fillGaps could connect 38 reactions (newConnected)
+%by including 42 reactions from the KEGG model (addedRxns). Those should of
+%course be checked manually to see that they exist in yeast, but in this
+%tutorial it is assumed that all of them occur in yeast.
+
+%Continue to improve the connectivity of the model by identifying
+%metabolites that should be connected. A convenient way to get an overview
+%of how connected the model is, and at the same time getting a lot of
+%useful data, is to use gapReport. Note that it can take several to many
+%hours to run, depending on the number of gaps in the model.
+[noFluxRxns, noFluxRxnsRelaxed, subGraphs, notProducedMets, minToConnect,...
+    neededForProductionMat]=gapReport(newModel);
+
+%The results show that 547 from 1620 reactions cannot carry flux. Remember
+%that an output is allowed for all metabolites, that is why it calculates
+%547 in both cases. 787 from 1614 metabolites cannot be synthesized from
+%the precursors which are supplied. There are 8 subnetworks in the model,
+%of which 1598 from 1614 metabolites belong to the first one.
+%
+%It will also print something similar to:
+%
+%To enable net production of all metabolites, a total of 304 metabolites
+%must be connected
+%Top 10 metabolites to connect:
+% 	1. 2,3-Dehydroacyl-CoA[s] (connects 144 metabolites)
+% 	2. 7,8-Dihydroneopterin[s] (connects 15 metabolites)
+% 	3. Thiamin diphosphate[s] (connects 13 metabolites)
+% 	4. Ergosterol[s] (connects 13 metabolites)
+% 	5. Methylselenic acid[s] (connects 12 metabolites)
+% 	6. G00003[s] (connects 12 metabolites)
+% 	7. Androstenediol[s] (connects 11 metabolites)
+% 	8. G00146[s] (connects 11 metabolites)
+% 	9. 1-Alkyl-2-acylglycerol[s] (connects 10 metabolites)
+% 	10. Progesterone[s] (connects 9 metabolites)
+
+%This is a very useful way of directing the gap-filling tasks to where they
+%are of the greatest use. In this case it says that in order to have net
+%production of all metabolites in the model from the simple inputs which
+%have been defined (687 metabolites can currently not have net production)
+%one needs to connect 304 unconnected metabolites in. However, by
+%connecting only the top 10 in the list 209/687 (30%) of them will now be
+%connected. When looking at the list one can see that the top ranking one
+%is 2,3-Dehydroacyl-CoA, which is involved in the elongation of many
+%different fatty acid chains. Metabolites 2 and 3 are co-factors or
+%involved in co- factor metabolism. It turns out that yeast cannot grow on
+%only the substrates have been defined, but that it requires some other
+%precursors for co-factor synthesis as well.
+
+%Add uptake reactions for the minimal media constituents needed for yeast
+%to grow.
+[~, J]=ismember({'4-Aminobenzoate';'Riboflavin';'Thiamine';'Biotin';'Folate';'Nicotinate';'Zymosterol';'Choline'},newModel.metNames);
+[newModel, addedRxns]=addExchangeRxns(newModel,'in',J);
+
+%Rerun gapReport and use the output for targeting the gap-filling efforts.
+%Note that only some info is printed; most of it is available in the output
+%structures. Work like this in an iterative manner until the model is of
+%sufficient quality.
