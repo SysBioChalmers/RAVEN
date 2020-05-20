1ac83fecdacbc252d9e95263d2241c147f821b7d
Cheewin Kittikunapong <cheewin@chalmers.se>
2020-05-20 07:54:48 +0700

    Update MAFFT for Windows
    Updated external binaries for MAFFT v. 7.467 for Windows OS

--- software/mafft/mafft-win/mafft.bat
+++ software/mafft/mafft-win/mafft.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 cls; 1>&2
+chcp 65001 1>&2
 for /f "usebackq tokens=*" %%i IN (`cd`) DO @set current_dir=%%i
 if /i "%current_dir%" == "%systemroot%" (
   set mafft_working_dir="%~dp0"
@@ -14,7 +15,6 @@ echo Preparing environment to run MAFFT on Windows. 1>&2
 echo This may take a while, if real-time scanning by anti-virus software is on. 1>&2
 
 set ROOTDIR=%~d0%~p0
-set ROOTDIRWBS=!ROOTDIR:^\\=\\\!
 set PATH=/usr/bin/:%PATH%
 set MAFFT_BINARIES=/usr/lib/mafft
 set TMPDIR=%TMP%
@@ -27,7 +27,7 @@ REM (typically C:\Users\username\AppData\Local\Temp\), then
 REM uncomment (remove REM) the above two lines to use an alternative 
 REM temporary folder.
 
-"%ROOTDIR%\usr\bin\bash" "%ROOTDIRWBS%\usr\bin\mafft" %*
+"%ROOTDIR%\usr\bin\bash" "/usr/bin/mafft" %*
 
 popd
 exit /b
