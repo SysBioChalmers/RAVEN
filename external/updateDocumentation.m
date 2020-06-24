cb7f284c5ae3c29c630f455b8db2161b4054c7e0
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2020-06-25 00:32:40 +0200

    feat: add updateDocumentation function
    This function does the automatic generation of HTML documentation for RAVEN *.m functions. An automatic removal of old HTML files was implemented. The list of RAVEN subdirectories is no longer hardcoded. Also ensured compatibility with Windows and Unix, macOS systems.

---
+++ external/updateDocumentation.m
@@ -0,0 +1,30 @@
+function updateDocumentation()
+% updateDocumentation
+%	Updates HTML documentation files for all RAVEN functions. It should
+%	only be used when preparing `devel` branch for a new RAVEN release
+%
+%	Usage: updateDocumentation()
+
+%Get the RAVEN path
+[ST, I]=dbstack('-completenames');
+[ravenDir,~,~]=fileparts(fileparts(ST(I).file));
+
+%Get a non-redundant list of RAVEN subdirectories containing MATLAB
+%functions. Absolute paths are not compatible with M2HTML, so convert them
+%to the relative paths instead.
+ravenDirs=regexprep(unique({dir(fullfile(ravenDir,'**/*.m')).folder}),'^.+RAVEN.{1,1}','');
+%Get rid of MATLAB functions from external software
+ravenDirs(:,contains(ravenDirs(1,:),'software'))=[];
+
+%Remove existing "doc" directory from RAVEN
+rmdir(fullfile(ravenDir,'doc'),'s');
+
+%Save the current working directory and go to RAVEN root directory
+originalDir=pwd;
+cd(ravenDir);
+%Generate HTML documentation files for RAVEN MATLAB functions
+m2html('mFiles',ravenDirs,'htmldir','doc');
+%Go back to the original working directory
+cd(originalDir);
+
+end
