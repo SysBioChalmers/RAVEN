5dadf3e3cabd5bb056346c14f3d91fd73ed6b462
Eduard Kerkhoven <eduardk@chalmers.se>
2021-05-10 22:09:54 +0200

    chore: track software folder in git LFS

--- software/m2html/@template/display.m
+++ software/m2html/@template/display.m
@@ -1,18 +1,3 @@
-function display(tpl)
-%TEMPLATE Display a template object in Matlab window
-%  DISPLAY(TPL) displays informations about the content of template 
-%  object TPL:
-%     Template Object: root '.', 2 files, 9 keys, comment unknowns.
-%  root element of template files, number of template files, number of 
-%  keywords defined and the way of handling unknowns tags.
-
-%  Copyright (C) 2003 Guillaume Flandin <Guillaume@artefact.tk>
-%  $Revision: 1.0 $Date: 2003/05/05 22:19:51 $
-
-disp(' ');
-disp([inputname(1),' = ']);
-disp(' ');
-for i=1:prod(size(tpl))
-	disp([blanks(length(inputname(1))+3) char(tpl(i))]);
-end
-disp(' ');
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f457eadf567374cbdd38cc8f49d06ed69ffcdc76751f1c47fd624d2ec2ffea2
+size 632
--- software/m2html/@template/private/loadtpl.m
+++ software/m2html/@template/private/loadtpl.m
@@ -1,24 +1,3 @@
-function tpl = loadtpl(tpl,handle)
-%TEMPLATE/LOADTPL Read a template from file
-%  TPL = LOADTPL(TPL,HANDLE) read the template file associated with the
-%  handle HANDLE in the template TPL and store it in the variable HANDLE.
-
-%  Copyright (C) 2003 Guillaume Flandin <Guillaume@artefact.tk>
-%  $Revision: 1.0 $Date: 2003/05/05 22:19:51 $
-
-if ~isempty(get(tpl,'var',handle))
-	return;
-else
-	ind = find(ismember(tpl.handles,handle));
-	if isempty(ind)
-		error('[Template] No such template handle.');
-	else
-		filename = tpl.file{ind};
-		[fid, errmsg] = fopen(filename,'rt');
-		if ~isempty(errmsg)
-			error(sprintf('Cannot open template file %s.',filename));
-		end
-		tpl = set(tpl,'var',handle,fscanf(fid,'%c'));
-		fclose(fid);
-	end
-end
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed0d11f187c4c1b99db1e6e6d6daabebb81e68790cfa45713914642afccdcee1
+size 754
