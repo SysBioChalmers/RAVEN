8c589efac88fe1a61ec972a4f246d04027d20fd5
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-11-14 11:17:29 +0100

    doc: update comments and restructure code in blastPlusTests

--- testing/unit_tests/blastPlusTests.m
+++ testing/unit_tests/blastPlusTests.m
@@ -34,6 +34,12 @@ else
     return
 end
 
+%Run BLAST multi-threaded to use all logical cores assigned to MATLAB
+cores = evalc('feature(''numcores'')');
+cores = strsplit(cores, 'MATLAB was assigned: ');
+cores = regexp(cores{2},'^\d*','match');
+cores = cores{1};
+
 %Create empty structures needed for actual MD5 hashes and BLAST results
 actDbHashes.phr={};
 actDbHashes.pot={};
@@ -42,20 +48,14 @@ actDbHashes.pto={};
 actBlastOutput={};
 actBlastStructure=[];
 
-%Create structures that contain expected MD5 hashes and BLAST results
+%Import structures that contain expected MD5 hashes and BLAST results
 sourceDir = fileparts(which(mfilename));
 load([sourceDir,'/test_data/expBlastResults.mat'],'expBlastStructure','expDbHashes');
 expBlastOutput = importdata([sourceDir,'/test_data/expBlastOutput.txt']);
 
 %Generate temporary names for working directory and outFile
 tmpDIR=tempname;
-outFile=tempname;
-
-%Run BLAST multi-threaded to use all logical cores assigned to MATLAB
-cores = evalc('feature(''numcores'')');
-cores = strsplit(cores, 'MATLAB was assigned: ');
-cores = regexp(cores{2},'^\d*','match');
-cores = cores{1};
+outFile=tempname;%BLAST results file
 
 %Create a temporary folder and copy multi-FASTA file there
 [~, ~]=system(['mkdir "' tmpDIR '"']);
@@ -63,14 +63,10 @@ copyfile(fullfile(sourceDir,'test_data','yeast_galactosidases.fa'),tmpDIR);
 
 %Construct a BLAST database
 [~, ~]=system(['"' fullfile(ravenPath,'software','blast+',['makeblastdb' binEnd]) '" -in "' fullfile(tmpDIR,'yeast_galactosidases.fa') '" -out "' fullfile(tmpDIR, 'yeast_galactosidases') '" -dbtype prot']);
-
 %Run a homology search
 [~, ~]=system(['"' fullfile(ravenPath,'software','blast+',['blastp' binEnd]) '" -query "' fullfile(tmpDIR,'yeast_galactosidases.fa') '" -out "' outFile '" -db "' fullfile(tmpDIR,'yeast_galactosidases') '" -evalue 10e-5 -outfmt "10 qseqid sseqid evalue pident length bitscore ppos" -num_threads "' cores '"']);
 
-
-
 %Generate actual hashing messages for BLAST+ database files
-
 switch binEnd
     case '.mac'
         [~, actPhrHashingMsg]=system(['md5 "' fullfile(tmpDIR,'yeast_galactosidases.phr') '"']);
@@ -89,13 +85,16 @@ switch binEnd
         [~, actPtoHashingMsg]=system(['certutil -hashfile "' fullfile(tmpDIR,'yeast_galactosidases.pto') '" MD5"']);
 end
 
+%Extract actual MD5 hashes for BLAST database files
 actDbHashes.phr = char(regexp(actPhrHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.pot = char(regexp(actPotHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.psq = char(regexp(actPsqHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.pto = char(regexp(actPtoHashingMsg,'[a-f0-9]{32}','match'));
+
+%Import BLAST results as raw text
 actBlastOutput = importdata(outFile);
 
-%Do the parsing of the text file
+%Do the parsing of the BLAST results text file
 actBlastStructure.fromId='sce';
 actBlastStructure.toId='sco';
 A=readtable(outFile,'Delimiter',',','Format','%s%s%f%f%f%f%f');
@@ -109,29 +108,28 @@ actBlastStructure.ppos=table2array(A(:,7));
 
 %Remove the old tempfiles
 delete([outFile '*']);
-
-%Remove temporary folder, since testing is finished
+%Remove temporary folder
 [~, ~]=system(['rm "' tmpDIR '" -r']);
 
 
-%Check if MD5 checksums for BLAST database are the same
+%Test 1a: Check if MD5 checksums for BLAST database files are the same
 verifyEqual(testCase,actDbHashes,expDbHashes);
 
-%Change one of the MD5 checksums and check if test fails
+%Test 1b: Change one of the MD5 checksums and check if test fails
 actDbHashes.phr=actDbHashes.pot;
 verifyNotEqual(testCase,actDbHashes,expDbHashes);
 
-%Check if BLAST result files are the same
+%Test 2a: Check if BLAST result files are the same
 verifyEqual(testCase,actBlastOutput,expBlastOutput);
 
-%Change actual BLAST result file and check if test fails
+%Test 2b: Change actual BLAST result file and check if test fails
 actBlastOutput='empty';
 verifyNotEqual(testCase,actBlastOutput,expBlastOutput);
 
-%Check if BLAST structures are the same
+%Test 3a: Check if BLAST structures are the same
 verifyEqual(testCase,actBlastStructure,expBlastStructure);
 
-%Modify actual BLAST structure and check if test fails
+%Test 3b: Modify actual BLAST structure and check if test fails
 actBlastStructure.toId=actBlastStructure.fromId;
 verifyNotEqual(testCase,actBlastStructure,expBlastStructure);
 end
