e92579d42810ab1d2028e7574a733024c2dd0c42
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-11-14 02:41:16 +0100

    fix: blastPlus tests modify BLAST results check

--- testing/unit_tests/blastPlusTests.m
+++ testing/unit_tests/blastPlusTests.m
@@ -10,7 +10,7 @@ function testBlastPlus(testCase)
 %    "phr", "pot", "psq" and "pto" files. The remaining files (i.e. "pdb",
 %    "pin" and "ptf") are not compared as these seem to be
 %    machine-specific.
-% 2. MD5 checksum check for BLAST result files. MD5 checksum check is not
+% 2. Raw text check for BLAST result files. MD5 checksum check is not
 %    amenable for these files since they seem to be machine-specific.
 % 3. Check of resulting blastStructure against the expected one. This is
 %    done to test BLAST results parsing in RAVEN.
@@ -38,12 +38,13 @@ actDbHashes.phr={};
 actDbHashes.pot={};
 actDbHashes.psq={};
 actDbHashes.pto={};
-actBlastOutputHash={};
+actBlastOutput={};
 actBlastStructure=[];
 
 %Create structures that contain expected MD5 hashes and BLAST results
 sourceDir = fileparts(which(mfilename));
-load([sourceDir,'/test_data/expBlastResults.mat'],'expBlastOutputHash','expBlastStructure','expDbHashes');
+load([sourceDir,'/test_data/expBlastResults.mat'],'expBlastStructure','expDbHashes');
+expBlastOutput = importdata([sourceDir,'/test_data/expBlastOutput.txt']);
 
 %Generate temporary names for working directory and outFile
 tmpDIR=tempname;
@@ -65,6 +66,8 @@ copyfile(fullfile(sourceDir,'test_data','yeast_galactosidases.fa'),tmpDIR);
 %Run a homology search
 [~, ~]=system(['"' fullfile(ravenPath,'software','blast+',['blastp' binEnd]) '" -query "' fullfile(tmpDIR,'yeast_galactosidases.fa') '" -out "' outFile '" -db "' fullfile(tmpDIR,'yeast_galactosidases') '" -evalue 10e-5 -outfmt "10 qseqid sseqid evalue pident length bitscore ppos" -num_threads "' cores '"']);
 
+
+
 %Generate actual hashing messages for BLAST+ database files
 
 switch binEnd
@@ -73,26 +76,23 @@ switch binEnd
         [~, actPotHashingMsg]=system(['md5 "' fullfile(tmpDIR,'yeast_galactosidases.pot') '"']);
         [~, actPsqHashingMsg]=system(['md5 "' fullfile(tmpDIR,'yeast_galactosidases.psq') '"']);
         [~, actPtoHashingMsg]=system(['md5 "' fullfile(tmpDIR,'yeast_galactosidases.pto') '"']);
-        [~, actOutFileHashingMsg]=system(['md5 "' outFile '"']);
     case ''
         [~, actPhrHashingMsg]=system(['md5sum "' fullfile(tmpDIR,'yeast_galactosidases.phr') '"']);
         [~, actPotHashingMsg]=system(['md5sum "' fullfile(tmpDIR,'yeast_galactosidases.pot') '"']);
         [~, actPsqHashingMsg]=system(['md5sum "' fullfile(tmpDIR,'yeast_galactosidases.psq') '"']);
         [~, actPtoHashingMsg]=system(['md5sum "' fullfile(tmpDIR,'yeast_galactosidases.pto') '"']);
-        [~, actOutFileHashingMsg]=system(['md5sum "' outFile '"']);
     case '.exe'
         [~, actPhrHashingMsg]=system(['certutil -hashfile "' fullfile(tmpDIR,'yeast_galactosidases.phr') '" MD5"']);
         [~, actPotHashingMsg]=system(['certutil -hashfile "' fullfile(tmpDIR,'yeast_galactosidases.pot') '" MD5"']);
         [~, actPsqHashingMsg]=system(['certutil -hashfile "' fullfile(tmpDIR,'yeast_galactosidases.psq') '" MD5"']);
         [~, actPtoHashingMsg]=system(['certutil -hashfile "' fullfile(tmpDIR,'yeast_galactosidases.pto') '" MD5"']);
-        [~, actOutFileHashingMsg]=system(['certutil -hashfile "' outFile '" MD5"']);
 end
 
 actDbHashes.phr = char(regexp(actPhrHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.pot = char(regexp(actPotHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.psq = char(regexp(actPsqHashingMsg,'[a-f0-9]{32}','match'));
 actDbHashes.pto = char(regexp(actPtoHashingMsg,'[a-f0-9]{32}','match'));
-actBlastOutputHash = char(regexp(actOutFileHashingMsg,'[a-f0-9]{32}','match'));
+actBlastOutput = importdata(outFile);
 
 %Do the parsing of the text file
 actBlastStructure.fromId='sce';
@@ -120,12 +120,12 @@ verifyEqual(testCase,actDbHashes,expDbHashes);
 actDbHashes.phr=actDbHashes.pot;
 verifyNotEqual(testCase,actDbHashes,expDbHashes);
 
-%Check if MD5 checksums for BLAST results are the same
-verifyEqual(testCase,actBlastOutputHash,expBlastOutputHash);
+%Check if BLAST result files are the same
+verifyEqual(testCase,actBlastOutput,expBlastOutput);
 
-%Change MD5 checksum and check if test fails
-actBlastOutputHash=actDbHashes.phr;
-verifyNotEqual(testCase,actBlastOutputHash,expBlastOutputHash);
+%Change actual BLAST result file and check if test fails
+actBlastOutput='empty';
+verifyNotEqual(testCase,actBlastOutput,expBlastOutput);
 
 %Check if BLAST structures are the same
 verifyEqual(testCase,actBlastStructure,expBlastStructure);
