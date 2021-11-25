c489d6d3158cc4a7b0ed8466be07a28c03f0d5d4
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-11-25 13:22:05 +0100

    fix: minor fixes in hmmerTests
    Yeast galactosidases are now queried against HMM trained using human galactosidases during the unit test for HMMER

--- testing/unit_tests/hmmerTests.m
+++ testing/unit_tests/hmmerTests.m
@@ -5,10 +5,23 @@ tests = functiontests(localfunctions);
 end
 
 function testHmmer(testCase)
+%This unit test comprises the functionality test for HMMER in RAVEN:
+% 1. Check of parsed HMMER results against the expected.
+%
+% NOTE: as hmm and HMMER results files are time-specific, no checks for
+% these files existence are done. Also, due to the way HMMER is utilized in
+% getKEGGModelForOrganism (HMMER result files can be parsed only once all
+% required hmm files are generated), the code segment involving HMMER
+% results parsing is pasted in this test function. Should the parsing problems
+% occur in the results processing, the code modifications shall be done in
+% this function and getKEGGModelForOrganism respectively.
+
+%%
 %Get the directory for RAVEN Toolbox
 [ST, I]=dbstack('-completenames');
 ravenPath=fileparts(fileparts(fileparts(ST(I).file)));
 
+%Identify the operating system
 if isunix
     if ismac
         binEnd='.mac';
@@ -28,7 +41,7 @@ actHmmResult.scores = [];
 
 %Create structures that contain expected HMMER results
 expHmmResult.genes = {'sp|P41947|MEL6_YEASX','sp|P41946|MEL5_YEASX', 'sp|P41945|MEL2_YEASX', 'sp|P04824|MEL1_YEASX'};
-expHmmResult.scores = [0, 0, 0, 0];
+expHmmResult.scores = [10^-250, 10^-250, 10^-250, 10^-250];
 
 %Generate temporary names for working directory and outFile
 tmpDIR=tempname;
@@ -45,22 +58,23 @@ cores = cores{1};
 
 sourceDir = fileparts(which(mfilename));
 copyfile(fullfile(sourceDir,'test_data','yeast_galactosidases.fa'),tmpDIR);
+copyfile(fullfile(sourceDir,'test_data','human_galactosidases.fa'),tmpDIR);
 
+%%
 %Train a hidden Markov model
-[~, ~]=system(['"' fullfile(ravenPath,'software','hmmer',['hmmbuild' binEnd]) '" --cpu "' num2str(cores) '" "' fullfile(tmpDIR,'yeast_galactosidases.hmm') '" "' fullfile(tmpDIR,'yeast_galactosidases.fa') '"']);
+[~, ~]=system(['"' fullfile(ravenPath,'software','hmmer',['hmmbuild' binEnd]) '" --cpu "' num2str(cores) '" "' fullfile(tmpDIR,'human_galactosidases.hmm') '" "' fullfile(tmpDIR,'yeast_galactosidases.fa') '"']);
 
 %Run a homology search against the newly-trained HMM
-[~, output]=system(['"' fullfile(ravenPath,'software','hmmer',['hmmsearch' binEnd]) '" --cpu "' num2str(cores) '" "' fullfile(tmpDIR,'yeast_galactosidases.hmm') '" "' fullfile(tmpDIR,'yeast_galactosidases.fa') '"']);
+[~, output]=system(['"' fullfile(ravenPath,'software','hmmer',['hmmsearch' binEnd]) '" --cpu "' num2str(cores) '" "' fullfile(tmpDIR,'human_galactosidases.hmm') '" "' fullfile(tmpDIR,'yeast_galactosidases.fa') '"']);
 
 %Save the output to a file
 fid=fopen(outFile,'w');
 fwrite(fid,output);
 fclose(fid);
 
-
+%%
 %Parse the results
-geneCounter=1;
-
+geneCounter=0;
 fid=fopen(outFile,'r');
 beginMatches=false;
 while 1
@@ -89,10 +103,21 @@ while 1
             elements=regexp(tline,' ','split');
             elements=elements(cellfun(@any,elements));
             
-            actHmmResult.scores(geneCounter)=str2double(elements{1});
-            actHmmResult.genes{geneCounter}=elements{9};
-            
-            geneCounter=geneCounter+1;
+            %Check if the match is below the treshhold
+            score=str2double(elements{1});
+            gene=elements{9};
+            if score<=10^-50
+                %If the score is exactly 0, change it to a very
+                %small value to avoid NaN
+                if score==0
+                    score=10^-250;
+                end
+                %Check if the gene is added already and, is so, get
+                %the best score for it
+                geneCounter=geneCounter+1;
+                actHmmResult.genes{geneCounter}=gene;
+                actHmmResult.scores(geneCounter)=score;
+            end
         else
             break;
         end
@@ -100,18 +125,17 @@ while 1
 end
 fclose(fid);
 
-
 %Remove the old tempfiles
 delete([outFile '*']);
 
 %Remove temporary folder, since testing is finished
 [~, ~]=system(['rm "' tmpDIR '" -r']);
 
-
-%Check if HMMER results match the expected ones
+%%
+%Test 1a: Check if HMMER results match the expected ones
 verifyEqual(testCase,actHmmResult,expHmmResult);
 
-%Modify actual HMMER results structure and check if test fails
+%Test 1b: Modify actual HMMER results structure and check if test fails
 actHmmResult.score(2)=1;
 verifyNotEqual(testCase,actHmmResult,expHmmResult);
 end
