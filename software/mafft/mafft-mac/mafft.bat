8f19eb59f75aff82d5d185ee20c5d5e9951ddcd1
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-12-18 13:27:44 +0100

    chore: remove all existing MAFFT files

--- software/mafft/mafft-mac/mafft.bat
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-pushd "`dirname "$0"`" > /dev/null 2>&1; rootdir="$PWD"; popd > /dev/null 2>&1;
-MAFFT_BINARIES="$rootdir/mafftdir/libexec"; export MAFFT_BINARIES;
-
-"$rootdir/mafftdir/bin/mafft" "$@"
-# $1 can have space in file name
