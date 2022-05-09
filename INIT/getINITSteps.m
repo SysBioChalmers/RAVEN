8c4d9f994a193c2d3a254fdc7a8c4cce18cad507
Johan Gustafsson <gustajo@chalmers.se>
2022-05-09 17:09:42 +0200

    featr: ftINIT: Added a chain of steps where step 3 is split into two steps. Not extensively tested.

--- INIT/getINITSteps.m
+++ INIT/getINITSteps.m
@@ -19,8 +19,10 @@ function steps = getINITSteps(metsToIgnore, series)
 %                                structural comparison, since the reactions removed
 %                                in step 3 may be a bit random and doesn't really
 %                                add any extra information.
-%                 'full' - 1-step run - similar to the old tINIT version, but 
+%                 'full' - 1-step run - similar to the old tINIT version but 
 %                          without simplifications. Very slow.
+%                 '4-step' - 4-step run - similar to default but the last 
+%                          step is split in two. Sometimes faster.
 %                 (opt, default 'default')
 %
 %   steps         Cell array of steps, used as input to ftINIT
@@ -47,7 +49,7 @@ if strcmp(series,'default') %the 3 step process described in the ftINIT paper
         INITStepDesc(false, false, 'exclude', [1,1,1,1,1,1,1,0], metsToIgnore, params, {10;20}); ...
         INITStepDesc(false, false, 'essential', [1,0,0,0,1,0,0,0], metsToIgnore, params, {10;20}); ...
         };
-elseif strcmp(series,'default') %the 3 step process described in the ftINIT paper
+elseif strcmp(series,'structural') %Skips step 3
     params1 = struct();
     params1.MIPGap = 0.0004;
     params1.TimeLimit = 120;
@@ -68,6 +70,20 @@ elseif strcmp(series,'full') %Just one run, slow on large models, but this is th
         INITStepDesc(false, false, 'ignore', [0,0,0,0,0,0,0,0], [], params) ...
         };
     
+elseif strcmp(series,'4-step') %the last step is split into two steps
+    params1 = struct();
+    params1.MIPGap = 0.0004;
+    params1.TimeLimit = 120;
+    params2 = struct();
+    params2.MIPGap = 0.0030;
+    params2.TimeLimit = 5000;
+    params = {params1;params2};
+    steps = { ...
+        INITStepDesc(true, true, 'ignore', [1,1,1,1,1,1,1,0], metsToIgnore, params, {10;20}); ...
+        INITStepDesc(false, false, 'exclude', [1,1,1,1,1,1,1,0], metsToIgnore, params, {10;20}); ...
+        INITStepDesc(false, false, 'essential', [1,1,0,0,1,0,0,0], metsToIgnore, params, {10;20}); ...
+        INITStepDesc(false, false, 'essential', [1,0,0,0,1,0,0,0], metsToIgnore, params, {10;20}); ...
+        };
 else
     dispEM(['Invalid series in getINITSteps: ' series])
 end
