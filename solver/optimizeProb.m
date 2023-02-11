9bdae7c52b5970eaea2525ff21fa77b9e538be50
edkerk <eduardk@chalmers.se>
2023-02-11 20:33:45 +0100

    refactor: optimizeProb smart ident in MATLAB

--- solver/optimizeProb.m
+++ solver/optimizeProb.m
@@ -45,7 +45,7 @@ defaultparams.intTol         = 1e-12;
 defaultparams.relMipGapTol   = 1e-12;
 defaultparams.absMipGapTol   = 1e-12;
 if milp
-    defaultparams.MIPGap     = 1e-12; 
+    defaultparams.MIPGap     = 1e-12;
     defaultparams.Seed       = 1;
 end
 
@@ -63,76 +63,76 @@ switch solver
             res.dual=-res.dual;
             res.rcost=-res.rcost;
         end
-        
-    %% Use Gurobi in a MATLAB environment
+
+        %% Use Gurobi in a MATLAB environment
     case 'gurobi'
-    if milp
-        if verbose
-            solverparams.OutputFlag = 1;
+        if milp
+            if verbose
+                solverparams.OutputFlag = 1;
+            else
+                solverparams.OutputFlag = 0;
+            end
+            solverparams.IntFeasTol = 10^-9; %min val for gurobi
+            solverparams.MIPGap = defaultparams.MIPGap;
+            solverparams.Seed = defaultparams.Seed;
         else
             solverparams.OutputFlag = 0;
         end
-        solverparams.IntFeasTol = 10^-9; %min val for gurobi
-        solverparams.MIPGap = defaultparams.MIPGap;
-        solverparams.Seed = defaultparams.Seed;
-    else
-        solverparams.OutputFlag = 0;
-    end
-    solverparams.DisplayInterval= 1; % Level of verbosity
-    solverparams.TimeLimit      = defaultparams.timeLimit;
-    solverparams.FeasibilityTol = defaultparams.feasTol;
-    solverparams.OptimalityTol  = defaultparams.optTol;
-    solverparams.Presolve       = 2;
-    solverparams = structUpdate(solverparams,params);
-    
-    % Restructering problem according to gurobi format
-    if isfield(prob, 'csense')
-        prob.sense = renameparams(prob.csense, {'L','G','E'}, {'<','>','='});
-        prob = rmfield(prob, {'csense'});
-    end
-    if isfield(prob, 'osense')
-        osense = prob.osense;
-        prob.modelsense = renameparams(num2str(prob.osense), {'1','-1'}, {'min','max'});
-        prob = rmfield(prob, {'osense'});
-    end
-    [prob.obj, prob.rhs, prob.vtype] = deal(prob.c, prob.b, prob.vartype);
-    prob = rmfield(prob, {'c','b','vartype'});
-    
-    resG = gurobi(prob,solverparams);
-    
-    try
-        % Name output fields the same as COBRA does
-        res.full     = resG.x;
-        res.obj      = resG.objval;
-        res.origStat = resG.status;
-        if isfield(resG,{'pi','rc'})
-            res.dual     = -resG.pi*osense;
-            res.rcost    = -resG.rc*osense;
+        solverparams.DisplayInterval= 1; % Level of verbosity
+        solverparams.TimeLimit      = defaultparams.timeLimit;
+        solverparams.FeasibilityTol = defaultparams.feasTol;
+        solverparams.OptimalityTol  = defaultparams.optTol;
+        solverparams.Presolve       = 2;
+        solverparams = structUpdate(solverparams,params);
+
+        % Restructering problem according to gurobi format
+        if isfield(prob, 'csense')
+            prob.sense = renameparams(prob.csense, {'L','G','E'}, {'<','>','='});
+            prob = rmfield(prob, {'csense'});
         end
-        if milp && strcmp(resG.status, 'TIME_LIMIT')
-            % If res has the objval field, it succeeded, regardless of
-            % time_limit status
-            resG.status = 'OPTIMAL';
+        if isfield(prob, 'osense')
+            osense = prob.osense;
+            prob.modelsense = renameparams(num2str(prob.osense), {'1','-1'}, {'min','max'});
+            prob = rmfield(prob, {'osense'});
         end
-        switch resG.status
-            case 'OPTIMAL'
-                res.stat = 1;
-            case 'UNBOUNDED'
-                res.stat = 2;
-            otherwise
-                res.stat = 0;
+        [prob.obj, prob.rhs, prob.vtype] = deal(prob.c, prob.b, prob.vartype);
+        prob = rmfield(prob, {'c','b','vartype'});
+
+        resG = gurobi(prob,solverparams);
+
+        try
+            % Name output fields the same as COBRA does
+            res.full     = resG.x;
+            res.obj      = resG.objval;
+            res.origStat = resG.status;
+            if isfield(resG,{'pi','rc'})
+                res.dual     = -resG.pi*osense;
+                res.rcost    = -resG.rc*osense;
+            end
+            if milp && strcmp(resG.status, 'TIME_LIMIT')
+                % If res has the objval field, it succeeded, regardless of
+                % time_limit status
+                resG.status = 'OPTIMAL';
+            end
+            switch resG.status
+                case 'OPTIMAL'
+                    res.stat = 1;
+                case 'UNBOUNDED'
+                    res.stat = 2;
+                otherwise
+                    res.stat = 0;
+            end
+            if ~milp
+                res.vbasis = resG.vbasis;
+                res.cbasis = resG.cbasis;
+    		else
+    			res.mipgap = resG.mipgap;
+            end
+        catch
+            res.stat = 0;
+            res.origStat = resG.status;  % useful information to have
         end
-        if ~milp
-            res.vbasis = resG.vbasis;
-            res.cbasis = resG.cbasis;
-		else
-			res.mipgap = resG.mipgap; 
-        end
-    catch
-        res.stat = 0;
-        res.origStat = resG.status;  % useful information to have
-    end
-    %% Use GLPK using RAVEN-provided binary
+        %% Use GLPK using RAVEN-provided binary
     case 'glpk'
         solverparams.scale   = 1; % Auto scaling
         solverparams.tmlim   = defaultparams.timeLimit;
@@ -142,22 +142,22 @@ switch solver
         solverparams.tolobj  = defaultparams.objTol;
         solverparams.msglev  = 0; % Level of verbosity
         solverparams = structUpdate(solverparams,params);
-        
+
         prob.csense = renameparams(prob.csense, {'L','G','E'}, {'U','L','S'});
-        
+
         if milp
             solverparams.tmlim   = solverparams.tmlim*10;
             solverparams.msglev  = 1; % Level of verbosity
             disp('Issues have been observed when using GLPK for MILP solving. Be advised to carefully observe the results, or us another solver.')
         end
-        
+
         % Ensure that RAVEN glpk binary is used, return to original
         % directory afterwards
         [ravenDir,currDir]=findRAVENroot();
         cd(fullfile(ravenDir,'software','GLPKmex'))
         [xopt, fmin, errnum, extra] = glpk(prob.c, prob.A, prob.b, prob.lb, prob.ub, prob.csense, prob.vartype, prob.osense, solverparams);
         cd(currDir)
-        
+
         switch errnum % 1 = undefined; 2 = feasible; 3 = infeasible; 4 = no feasible solution; 5 = optimal; 6 = no unbounded solution
             case 5
                 res.stat = 1; % Optimal
